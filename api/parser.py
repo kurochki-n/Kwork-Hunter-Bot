118b804a14a621e9c3ed61d2e34cc6e8409a173e
Kurochkin Egor <willson2019.octa@mail.ru>
2024-02-29 15:09:37 +0400

    update

--- api/parser.py
+++ api/parser.py
@@ -1,14 +1,45 @@
-import requests
+import aiohttp
 from bs4 import BeautifulSoup
-
-from loader import loop
-
-
-bs = BeautifulSoup()
-
-async def parse_kwork():
-    pass
+from threading import Thread
+from fake_useragent import UserAgent
 
 
-async def run_parse():
-    loop.create_task(parse_kwork())
+class KworkParser():
+    
+    def __init__(self, session) -> None:
+        self.session = session
+        self.headers = {
+            "Accept": "text/html",
+            "User-Agent": UserAgent().random
+            }
+
+
+    async def get_categories(self):
+        categories = []
+        async with self.session.get(url="https://kwork.ru/manage_kworks", headers=self.headers) as response:
+            soup = BeautifulSoup(response, "lxml")
+            result = soup.find('//*[@id="body"]/div[6]/div[1]/div[2]/div[1]/ul[1').text
+            print(result)
+        return categories
+    
+    
+    async def get_subcategories(self, category):
+        subcategories = []
+        return subcategories
+    
+    
+    async def parse_kwork(self, message, subcategory):
+        pass
+        # async with self.session.get(url="https://service.urentbike.ru/gatewayclient/api/v1/payment/profile",
+        #                                 headers=(access_headers or self.access_headers)) as response_payment_profile:
+        #         return await response_payment_profile.json()
+
+
+async def run_parse(message):
+    async with aiohttp.ClientSession() as session:
+        parser = KworkParser(session=session)
+        for cat in await parser.get_categories():
+            for subcat in await parser.get_subcategories(cat):
+                th = Thread(target=parser.parse_kwork, args=(message, subcat,))
+                th.start()
+        
