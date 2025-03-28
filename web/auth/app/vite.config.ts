1cd14fd9e341eab6148e449549a949c137e251ec
Kurochkin Egor <willson2019.octa@mail.ru>
2025-03-28 23:16:10 +0400

    upd

--- web/auth/app/vite.config.ts
+++ web/auth/app/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/auth': {
-        target: 'https://projecthunter-webhook.ru.tuna.am',
+        target: 'https://domain.com',
         changeOrigin: true
       }
     }
